b5affb41979c296689facaaa924192f1db9804b8
Emma Dayton <emma.dayton85@gmail.com>
2019-03-08 14:24:16 -0800

    finish index formatting, begin request html fields and python section

--- dojo_survey.py
+++ dojo_survey.py
@@ -6,5 +6,11 @@ def index():
     return render_template("index.html")
 
 
+@app.route('/results', methods=['POST'])
+def results():
+    name = request.form['name']
+    # location = request.form['location']
+    return render_template("index.html", name=name)
+
 if __name__ == "__main__":
     app.run(debug=True)
