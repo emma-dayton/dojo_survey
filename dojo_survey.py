9c6489eb17f1c8c5a52eac62273d05c41abc4d90
Emma Dayton <emma.dayton85@gmail.com>
2019-03-08 14:59:27 -0800

    finish html styling of results page and results python page

--- dojo_survey.py
+++ dojo_survey.py
@@ -9,8 +9,11 @@ def index():
 @app.route('/results', methods=['POST'])
 def results():
     name = request.form['name']
-    # location = request.form['location']
-    return render_template("index.html", name=name)
+    location = request.form.get('location')
+    language = request.form.get('language')
+    comment = request.form['comment']
+
+    return render_template("index.html", name=name, location=location, language=language, comment=comment)
 
 if __name__ == "__main__":
     app.run(debug=True)
